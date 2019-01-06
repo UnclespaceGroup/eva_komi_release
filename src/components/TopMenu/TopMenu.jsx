713671acb4695fe854be5f962c0087385dff32d8
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2019-01-06 16:16:27 +0300

    burger menu

--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -11,26 +11,61 @@ const t = {
     {
       title: 'Быстрый заказ эвакуатора',
       href: 'tel: 340111'
+    },
+    {
+      title: 'Цены за вызов',
+      href: 'tel: 340111'
+    },
+    {
+      title: 'Об эвакуации',
+      href: 'tel: 340111'
     }
   ]
 }
 
 class TopMenu extends Component {
+  state = {
+    open: false
+  }
   render(){
     const {
       items
     } = t
+    const {
+      state: {
+        open
+      },
+      click
+    } = this
     return(
       <div className={s.container}>
         <div className={s.items}>
           {
+            isMobile ? open &&
             _.map(items, ({title, href}, key) =>
-            <a key={key} href={href} >{title}</a>
+            <a className={s.item} key={key} href={href} >{title}</a>
             )
+              :
+              _.map(items, ({title, href}, key) =>
+                <a className={s.item} key={key} href={href} >{title}</a>
+              )
+          }
+          {
+            isMobile ?
+              <div className={s.top}>
+                <a href={'tel: 340111'} className={s.button}>Быстрый вызов</a>
+                <div className={s.burger} onClick={click}/>
+              </div>
+
+              :<a href={'tel: 340111'} className={s.button}>Быстрый вызов</a>
           }
         </div>
       </div>
     )
+  }
+  click = () => {
+    const { open } = this.state
+    this.setState({open: !open})
   }
 }
 export default TopMenu
