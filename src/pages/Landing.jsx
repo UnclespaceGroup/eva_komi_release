86782ca4a65866bdbb27e07491224ff111c9ceab
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2019-01-06 14:59:33 +0300

    lodash

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import desktop from './css/desktop.module.scss'
 import mobile from './css/mobile.module.scss'
 import { isMobile } from "react-device-detect";
+import TopMenu from '../components/TopMenu/TopMenu'
 
 const s = isMobile ? mobile : desktop
 
@@ -9,7 +10,7 @@ class Landing extends Component {
   render(){
     return(
       <div className={s.container}>
-        dvmkm
+        <TopMenu />
       </div>
     )
   }
