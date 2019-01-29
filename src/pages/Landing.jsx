5cf0c794590f429935155a29eb8143797a0566b7
IvanDmitrachkov <idmitrachkovivan@aic.ru>
2019-01-29 19:39:31 +0300

    fix

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,29 +1,31 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import desktop from './css/desktop.module.scss'
 import mobile from './css/mobile.module.scss'
-import {isMobile} from "react-device-detect";
+import { isMobile } from 'react-device-detect'
 import TopMenu from '../components/TopMenu/TopMenu'
-import TopPage from "../components/topPage/TopPage";
-import Footer from "../components/Footer/Footer";
+import TopPage from '../components/topPage/TopPage'
+import Footer from '../components/Footer/Footer'
 import Price from '../components/Price/Price'
 import Advantages from '../components/Advantages/Advantages'
 import Banner from '../components/Banners/Banner'
-import Description from "../components/Description/Description";
+import Description from '../components/Description/Description'
 
 const s = isMobile ? mobile : desktop
 
 class Landing extends Component {
-  render(){
-    return(
+  render () {
+    return (
       <div className={s.container}>
         <TopMenu />
         <TopPage />
         <Advantages />
         <div id="price" />
-        <Banner/>
+
         <Price />
-          <Description />
-        <Footer/>
+
+        <Banner />
+        <Description />
+        <Footer />
       </div>
     )
   }
